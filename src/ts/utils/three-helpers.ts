850cad53ce38523d802c538697322d61778b533a
m0wh <malo.dewiderspach@gmail.com>
2023-06-27 23:10:58 +0200

    mobile

--- src/ts/utils/three-helpers.ts
+++ src/ts/utils/three-helpers.ts
@@ -2,21 +2,30 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import raf from './raf'
 
+let w, h
+if (window.innerWidth > window.innerHeight) {
+  w = window.innerWidth / 2
+  h = window.innerHeight
+} else {
+  w = window.innerWidth
+  h = window.innerHeight / 2
+}
+
 export function init () {
   const scene = new THREE.Scene()
-  const camera = new THREE.PerspectiveCamera(50, window.innerWidth / 2 / window.innerHeight, 0.1, 100)
+  const camera = new THREE.PerspectiveCamera(50, w / h, 0.1, 100)
   const renderer = new THREE.WebGLRenderer({ antialias: true })
   const controls = new OrbitControls(camera, renderer.domElement)
 
-  renderer.setSize(window.innerWidth / 2, window.innerHeight)
+  renderer.setSize(w, h)
   document.getElementById('view').appendChild(renderer.domElement)
 
   controls.enableDamping = true
 
   window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / 2 / window.innerHeight
+    camera.aspect = w / h
     camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth / 2, window.innerHeight)
+    renderer.setSize(w, h)
   })
 
   raf.subscribe(() => {
